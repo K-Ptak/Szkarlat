e252caa8c60179c71c18ad08f90b749a63abae74
Kamil Ptak <kamil2001p@op.pl>
2021-10-20 14:59:12 +0200

    Dodaj pliki projektów.

---
+++ Szkarłat.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include "include/color.hpp"
+using namespace std;
+
+int main()
+{
+    int a;
+    for (;;) {
+        cout << dye::red("Hello World!") << endl;
+        cout << dye::light_red("Hello World!") << endl;
+        cout << dye::yellow("Hello World!") << endl;
+        cout << dye::green("Hello World!") << endl;
+        cout << dye::blue("Hello World!") << endl;
+        cout << dye::aqua("Hello World!") << endl;
+        cout << dye::purple("Hello World!") << endl;
+        
+    }
+}
